8b565983824c571111350ab3da93dfa32141d723
Yago de Martin <yamartin@emeal.nttdata.com>
2023-12-28 11:09:43 +0100

    WIP: Primera versión del paquete.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ToastPackage",
+    platforms: [.iOS(.v16)],
+
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ToastPackage",
+            targets: ["ToastPackage"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ToastPackage"),
+        .testTarget(
+            name: "ToastPackageTests",
+            dependencies: ["ToastPackage"]),
+    ]
+)
